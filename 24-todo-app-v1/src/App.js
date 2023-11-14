810a7174532a6c9c42900a538a3ee69540083aab
PenkovS <penkov_serezha05@mail.ru>
2023-11-14 15:14:06 +0300

    icon

--- 24-todo-app-v1/src/App.js
+++ 24-todo-app-v1/src/App.js
@@ -5,14 +5,20 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
+
   const addTodoHandler = (text) => {
-    setTodos([...todos,text])
+    setTodos([...todos, text])
   }
+
+  const deleteTodoHandler = (index) => {
+    setTodos(todos.filter((_, idx) => idx !== index));
+  } 
+
   return (  
     <div className="App"> 
       <h1>Мои Заметки</h1>
-      <TodoForm addTodo={addTodoHandler}/>
-      <TodoList todos={todos}/>
+      <TodoForm addTodo={addTodoHandler} />
+      <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
     </div>
   )
 }
--- 24-todo-app-v1/src/component/Todos/TodoForm.js
+++ 24-todo-app-v1/src/component/Todos/TodoForm.js
@@ -1,23 +1,28 @@
-import { useState } from "react"
-
+import { useState } from "react";
+import styles from './TodoForm.module.css';
+console.log(styles);
 
 function TodoForm({ addTodo}) {   
     const [text, setText] = useState('')
 
     function onSubmitHandler(event) {
-        event.preventDefault()
-        addTodo(text)
-        setText('')
+        event.preventDefault();
+        addTodo(text);
+        setText('');
     }
-    return (    
-        <form onSubmit={onSubmitHandler}>  
-            <input
-             placeholder="Введите новую заметку"
-             value={text}
-             onChange={(e) => setText(e.target.value)}/>
-            <button type="submit">Добавить</button>
 
-        </form>
+    return (
+        <div className={styles.todoFormContainer}>   
+            <form onSubmit={onSubmitHandler}>  
+                <input
+                placeholder="Введите новую заметку"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                />
+                <button type="submit">Добавить</button>
+            </form>
+        </div> 
     )
 }
+
 export default TodoForm
